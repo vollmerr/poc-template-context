83b2906e0afad43eeba02200cd8321d2e98e493b
vollmerr <vollmerr89@gmail.com>
2022-02-17 10:57:50 -0800

    add react ga

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,8 +1,13 @@
+import ga from "react-ga";
+
+const libraryUrl = "https://www.google-analytics.com/analytics_debug.js";
+
 const debug = 1;
 const log = debug ? console.log : () => {};
 
 export const initialize = () => {
   log("[analytics] initialize");
+  ga.initialize("UA-217141661-3");
 };
 
 export const setAppName = (appName) => {
@@ -11,10 +16,12 @@ export const setAppName = (appName) => {
 
 export const sendLoaded = () => {
   log("[analytics] sending loaded");
+  ga.timing({ category: 'test-cat', value: 123, variable: 'test-var' })
 };
 
 export const sendPageView = (page) => {
   log("[analytics] sending pageview: ", { page });
+  ga.pageview(page);
 };
 
 export const sendEvent = (...event) => {
